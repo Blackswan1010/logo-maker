9e789e3f7593c1dfd7fe176ceeb894b1278ff706
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-23 20:34:41 -0700

    add packages, add developer comments, add function to alert the user of file generation, add classes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // Importing required packages
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Square, Circle } = require('./lib/shape');
 
-// Array of prompt questions for the user
+// Create an array of prompt questions for the user
+// Questions to ask user for: 3 letters, text color, list of shapes, shape color
 const questions = [
     {
-        message: 'Enter 3 letters you wish for your logo.',
+        message: 'Enter 3 letters for your logo.',
         type: 'input',
         name: 'letters',
+        validate: (letters) => {
+            if(!letters || letters.length > 3 || letters.length < 3){
+                return 'Please provide 3 characters';
+            }
+            return true;
+        }
     },
     {
         message: 'Enter a text color.',
@@ -14,25 +23,41 @@ const questions = [
         name: 'textColor',
     },
     {
-        message: 'Enter a shape for your logo.',
+        message: 'Choose a shape for your logo.',
         type: 'list',
-        name: ['triangle', 'circle', 'square'],
+        name: 'shape',
+        choices: ['triangle', 'circle', 'square'],
     },
     {
         message: 'Enter a color for your shape.',
         type: 'input',
-        name: 'shapeColor',
+        name: 'shapeColor'
     }
 ];
 
+// Create a function to write an SVG file
+const writeToFile = (fileName, data) => {
+    // Use FS module to write data and fileName to svg file
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Generated logo.svg!')
+    );
+}
 
+// Create a function to generate svg logo
+const generateLogo = (data) => {
+
+    return;
+}
+
+// Create a function to initialize app
 init = () => {
     inquirer
     .prompt(questions)
     .then((response) => {
-        
+        writeToFile('./results/logo.svg', generateLogo(response));
     })
 }
 
-
+// Function call to initialize app
+// Use inquirer to display questions to user
 init();
